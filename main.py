dfb7fac51352db041b1f60f78dd394e5dc93cc7a
Qiyang Hu <qhu@qiyang-wrk.local>
2022-03-15 16:05:06 +0100

    add about page

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
