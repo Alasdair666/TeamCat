55b1eea206885f5f61283f88555c8609f21c47f1
Alexandre Tarantini <alexandre.tarantini@inait.ai>
2022-03-16 14:08:18 +0100

    Add diet page

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,3 +1,4 @@
+# Homepage
 def test_access_homepage(client):
     resp = client.get("/")
     assert resp.status_code == 200
@@ -8,6 +9,7 @@ def test_homepage_is_cat_related(client):
     assert b"cat" in resp.data
 
 
+# About page
 def test_access_aboutpage(client):
     resp = client.get("/about")
     assert resp.status_code == 200
@@ -18,6 +20,7 @@ def test_aboutpage_mentions_one_of_us(client):
     assert b"qiyang" in resp.data.lower()
 
 
+# Contact page
 def test_access_contactpage(client):
     resp = client.get("/contact")
     assert resp.status_code == 200
@@ -28,6 +31,18 @@ def test_contactpage_has_mailto(client):
     assert b"mailto:" in resp.data.lower()
 
 
+# Diet page
+def test_access_dietpage(client):
+    resp = client.get("/diet")
+    assert resp.status_code == 200
+
+
+def test_dietpage_mentions_diets(client):
+    resp = client.get("/diet")
+    assert b"diets" in resp.data.lower()
+
+
+# 404 Non existing
 def test_access_non_existing_page(client):
     resp = client.get("/no_page")
     assert resp.status_code == 404
